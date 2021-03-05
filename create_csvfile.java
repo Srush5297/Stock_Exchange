1133653e94bf26327ea63cd6891accc85f491ba6
Srush5297 <42892917+Srush5297@users.noreply.github.com>
2021-03-05 08:55:01 +0530

    Add files via upload

---
+++ create_csvfile.java
@@ -0,0 +1,28 @@
+package Data;
+
+import java.io.PrintWriter;
+
+public class create_csvfile 
+{
+	public static void main(String [] args)
+	{
+		try
+		{
+			PrintWriter pw = new PrintWriter("C:/Users/MY PC/Desktop/SinglCsv.csv");
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("Symbol");
+			sb.append("Daily_Range");
+			sb.append("DATR");
+			
+			pw.write(sb.toString());
+			pw.close();
+			System.out.println("Finished");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
